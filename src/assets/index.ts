268fa6874fbb904c575389bbfd06c49c946a2deb
thaisnevest <thaisneves@citi.org.br>
2024-10-03 21:24:48 -0300

    feat(components): added assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,7 @@ import People from './People.svg';
 import UserCircle from './UserCircle.svg';
 import House from './House.svg';
 import GameController from './GameController.svg';
+import MadeWith from './MadeWith.svg';
+import LogoCiti from './LogoCiti.svg';
 
-export { Logo, People, UserCircle, House, GameController };
+export { Logo, People, UserCircle, House, GameController, LogoCiti, MadeWith };
